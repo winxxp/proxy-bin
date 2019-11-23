3ab654e8bbf9a0404bc085455e71fd4363422a36
winxxp <haolei2008@gmial.com>
2019-11-23 03:50:49 +0000

    add server

--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-nohup ./go-shadowsocks2 -s 'ss://AES-128-CTR:16777216@:11111' &
+cd /home/winxxp/proxy-bin
+nohup /home/winxxp/proxy-bin/go-shadowsocks2 -s 'ss://AES-128-CTR:16777216@:8080' > /dev/zero &
+nohup /home/winxxp/proxy-bin/go-shadowsocks2 -s 'ss://AES-128-CTR:16777216@:11111' > /dev/zero &
+nohup /home/winxxp/proxy-bin/go-shadowsocks2 -s 'ss://AES-128-CTR:16777216@:22222' > /dev/zero &
+nohup /home/winxxp/proxy-bin/go-shadowsocks2 -s 'ss://AES-128-CTR:16777216@:33333' > /dev/zero &
